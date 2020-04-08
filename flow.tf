24a6b0bee6b24fa9f2f80cf46c5e5507b7f55e04
Phil Dier <phil@raventools.com>
2020-04-08 17:48:53 -0500

    update to tf 0.12

--- flow.tf
+++ flow.tf
@@ -1,6 +1,7 @@
 resource "aws_flow_log" "main" {
-    vpc_id = "${var.vpc_id}"
-    log_destination = "${aws_cloudwatch_log_group.vpcflow.arn}"
-    iam_role_arn = "${aws_iam_role.vpcflow.arn}"
-    traffic_type = "${var.traffic_type}"
+  vpc_id          = var.vpc_id
+  log_destination = aws_cloudwatch_log_group.vpcflow.arn
+  iam_role_arn    = aws_iam_role.vpcflow.arn
+  traffic_type    = var.traffic_type
 }
+
--- vars.tf
+++ vars.tf
@@ -1,19 +1,20 @@
 variable "app" {
-    type = "string"
-    description = "The application name, used in the `Application` tags."
+  type        = string
+  description = "The application name, used in the `Application` tags."
 }
 
 variable "env" {
-    type = "string"
-    description = "The application environment (prod, staging, etc). Pass in terraform.env here if you like. Used in the `Environment` tags."
+  type        = string
+  description = "The application environment (prod, staging, etc). Pass in terraform.env here if you like. Used in the `Environment` tags."
 }
 
 variable "vpc_id" {
-    type = "string"
-    description = "The VPC to be logged"
+  type        = string
+  description = "The VPC to be logged"
 }
 
 variable "traffic_type" {
-    type = "string"
-    description = "The traffic type to track (ACCEPT, REJECT, ALL)"
+  type        = string
+  description = "The traffic type to track (ACCEPT, REJECT, ALL)"
 }
+
